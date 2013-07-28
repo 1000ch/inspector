a3c50d6ea8137397f058451c61cc7bad10c924e8
1000ch <orz1000ch@gmail.com>
2013-07-28 18:41:37 +0900

    implement #2

--- src/js/background.js
+++ src/js/background.js
@@ -1,39 +1,55 @@
 (function() {
 	//this is global
-	var settingKey = "inspector.isRunning";
-	var isRunning = localStorage.getItem(settingKey) || true;
-	console.log("background.js is executed");
-	console.log("settingKey is " + localStorage.getItem(settingKey));
-	changeIconState(isRunning);
+	var settings = {isRunning: true};
+	chrome.storage.local.get(Object.keys(settings), function(items) {
+		if(items.isRunning === undefined) {
+			chrome.storage.local.set(settings, function() {
+				//here is asynchronous i/o though, don't mind.
+			});
+		} else {
+			settings.isRunning = items.isRunning;
+		}
+		changeIconState(settings.isRunning);
+	});
 
 	chrome.tabs.onActivated.addListener(function(activeInfo) {
-		console.log("chrome.tabs.onActivated");
 		//if active tab is changed
-		changeIconState(isRunning);
+		changeIconState(settings.isRunning);
+
+		var message = {
+			runningState: settings.isRunning
+		};
+
+		//update contentScript setting
+		chrome.tabs.sendMessage(activeInfo.tabId, message, noop);
 	});
 
 	chrome.browserAction.onClicked.addListener(function(tab) {
 		if(tab) {
 			//toggle flag to inspect
-			isRunning = !isRunning;
+			settings.isRunning = !settings.isRunning;
 
 			//save the flag
-			localStorage.setItem(settingKey, isRunning);
+			chrome.storage.local.set(settings, noop);
 
 			//change icon state
-			changeIconState(isRunning);
+			changeIconState(settings.isRunning);
 
 			//if state is changed to inspect, send message
-			if(isRunning) {
-				var message = {
-					command: "inspect"
-				};
-				chrome.tabs.connect(tab.id);
-				chrome.tabs.sendMessage(tab.id, message, function(response) {});
-			}
+			var message = {
+				runningState: settings.isRunning,
+				command: "inspect"
+			};
+
+			//send message
+			chrome.tabs.sendMessage(tab.id, message, noop);
 		}
 	});
 
+	/**
+	 * set extension badge
+	 * @param {Boolean} isRunning
+	 */
 	function changeIconState(isRunning) {
 		if(isRunning) {
 			chrome.browserAction.setIcon({
@@ -51,4 +67,10 @@
 			});
 		}
 	}
+
+	/**
+	 * noop
+	 */
+	function noop() {}
+
 })();
--- src/js/content.js
+++ src/js/content.js
@@ -1,15 +1,35 @@
 (function() {
-	console.log("content.js");
-	var option = settings || {};
+	//todo
+	var inspectorOption = settings || {};
+
+	//inspector state of contentScript
+	var isRunning = true;
+
+	//listen message
 	chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-		if(message.command === "inspect" && HTMLInspector) {
-			HTMLInspector.inspect(option);
+		isRunning = !!message.runningState;
+		if(isRunning) {
+			if(message.command === "inspect" && HTMLInspector) {
+				HTMLInspector.inspect(inspectorOption);
+			}
 		}
 	});
-	document.addEventListener("DOMContentLoaded", function() {
-		console.log("DOMContentLoaded");
-		if(HTMLInspector) {
-			HTMLInspector.inspect(option);
+	var stateArray = ["complete", "loaded", "interactive"];
+	chrome.storage.local.get(["isRunning"], function(items) {
+		isRunning = !!items.isRunning;
+		if(isRunning) {
+			if(stateArray.indexOf(document.readyState) !== -1) {
+				if(HTMLInspector) {
+					HTMLInspector.inspect(inspectorOption);
+				}
+			} else {
+				document.addEventListener("DOMContentLoaded", function() {
+					console.log("DOMContentLoaded");
+					if(HTMLInspector) {
+						HTMLInspector.inspect(inspectorOption);
+					}
+				});
+			}
 		}
 	});
 })();
