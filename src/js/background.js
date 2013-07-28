ede6af1e1507f6bcb2222a9d628d374aa66fea82
1000ch <orz1000ch@gmail.com>
2013-07-28 13:23:19 +0900

    toggle icon and define on/off flag to inspect

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,54 @@
 (function() {
+	//this is global
+	var settingKey = "inspector.isRunning";
+	var isRunning = localStorage.getItem(settingKey) || true;
+	console.log("background.js is executed");
+	console.log("settingKey is " + localStorage.getItem(settingKey));
+	changeIconState(isRunning);
+
+	chrome.tabs.onActivated.addListener(function(activeInfo) {
+		console.log("chrome.tabs.onActivated");
+		//if active tab is changed
+		changeIconState(isRunning);
+	});
+
 	chrome.browserAction.onClicked.addListener(function(tab) {
 		if(tab) {
-			var message = {
-				command: "inspect"
-			};
-			chrome.tabs.connect(tab.id);
-			chrome.tabs.sendMessage(tab.id, message, function(response) {});
+			//toggle flag to inspect
+			isRunning = !isRunning;
+
+			//save the flag
+			localStorage.setItem(settingKey, isRunning);
+
+			//change icon state
+			changeIconState(isRunning);
+
+			//if state is changed to inspect, send message
+			if(isRunning) {
+				var message = {
+					command: "inspect"
+				};
+				chrome.tabs.connect(tab.id);
+				chrome.tabs.sendMessage(tab.id, message, function(response) {});
+			}
 		}
 	});
+
+	function changeIconState(isRunning) {
+		if(isRunning) {
+			chrome.browserAction.setIcon({
+				path: "/src/icon/badge_on.png"
+			}, function() {});
+			chrome.browserAction.setBadgeText({
+				text: "on"
+			});
+		} else {
+			chrome.browserAction.setIcon({
+				path: "/src/icon/badge_off.png"
+			}, function() {});
+			chrome.browserAction.setBadgeText({
+				text: "off"
+			});
+		}
+	}
 })();
--- src/js/content.js
+++ src/js/content.js
@@ -1,7 +1,14 @@
 (function() {
+	console.log("content.js");
 	var option = settings || {};
 	chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 		if(message.command === "inspect" && HTMLInspector) {
+			HTMLInspector.inspect(option);
+		}
+	});
+	document.addEventListener("DOMContentLoaded", function() {
+		console.log("DOMContentLoaded");
+		if(HTMLInspector) {
 			HTMLInspector.inspect(option);
 		}
 	});
